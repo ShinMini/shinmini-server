69a82a7e5af637c7b446ede3c8b5fb29d8a9f72e
HyeonMin Shin <gusals121234@gmail.com>
2023-09-06 14:39:13 +0900

    update: env setup

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(process.env.PORT || 3000)
 }
-bootstrap()
+bootstrap().then(() =>
+  console.log(
+    `Application is successfully running on ${process.env.PORT || 3000} port`
+  )
+)
