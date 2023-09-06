aa94ba4ed50b921e5c3d163167be2c682b9a4a62
HyeonMin Shin <gusals121234@gmail.com>
2023-09-06 16:01:43 +0900

    merge: connect with shinmini-homepage

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { Public } from './auth/auth.decorator'
 
-@Controller()
+@Controller('shinmini-homepage/us-central1/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @Public()
+  @Get('test')
+  connectTesting(): string {
+    return this.appService.connectTesting()
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,17 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [
+        'http://localhost:3000',
+        'https://shinmini.com',
+        'https://shinmini-homepage.web.app',
+        'https://shinmini-homepage.firebaseapp.com',
+      ],
+      credentials: true,
+    },
+  })
   await app.listen(process.env.PORT || 3000)
 }
 bootstrap().then(() =>
